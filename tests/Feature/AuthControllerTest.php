cdc0e0d4bfb156144a58c918a0b273ecfb9bf231
Jeanne Michel <jeanne.michel@he-arc.ch>
2023-06-05 16:40:37 +0200

    added test to test test coverage in sonarcloud

---
+++ tests/Feature/AuthControllerTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class AuthControllerTest extends TestCase
+{
+
+    public function test_register(): void
+    {
+        $response = $this->postJson('/api/auth/register', [
+            'name' => 'John Doe',
+            'email' => 'john.doe@exemple.com',
+            'password' => 'thisisasupersecurepassword',
+            'password_confirmation' => 'thisisasupersecurepassword',
+            'device_name' => 'test'
+        ]);
+
+        $response->assertStatus(201);
+    }
+}
