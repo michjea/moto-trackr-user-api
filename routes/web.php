c327b2ab0924fe1dee3ed29427e574545f43ae42
Jeanne Michel <jeanne.michel@he-arc.ch>
2023-07-11 18:35:37 +0200

    test

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'message' => 'Home route',
+    ], 200);
 });
 
 // test route
